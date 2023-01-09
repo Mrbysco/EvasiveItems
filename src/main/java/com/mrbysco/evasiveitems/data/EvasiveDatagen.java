cd9ef44293dc13eef34f4eaeeae24ab37f033871
Mrbysco <Mrbysco@users.noreply.github.com>
2023-01-09 22:22:21 +0100

    Port to 1.19.2 [push]

--- src/main/java/com/mrbysco/evasiveitems/data/EvasiveDatagen.java
+++ src/main/java/com/mrbysco/evasiveitems/data/EvasiveDatagen.java
@@ -4,9 +4,9 @@ import com.mrbysco.evasiveitems.data.client.EvasiveLanguageProvider;
 import com.mrbysco.evasiveitems.data.client.EvasiveSoundProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EvasiveDatagen {
@@ -16,8 +16,8 @@ public class EvasiveDatagen {
 		ExistingFileHelper helper = event.getExistingFileHelper();
 
 		if (event.includeClient()) {
-			generator.addProvider(new EvasiveLanguageProvider(generator));
-			generator.addProvider(new EvasiveSoundProvider(generator, helper));
+			generator.addProvider(event.includeClient(), new EvasiveLanguageProvider(generator));
+			generator.addProvider(event.includeClient(), new EvasiveSoundProvider(generator, helper));
 		}
 	}
 }
